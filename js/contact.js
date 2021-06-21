bc9555fcc28eade4eb2d068fc01e7d2fadc301d6
Mervenur <ylmz.merwusch@gmail.com>
2021-06-22 00:08:03 +0300

    Contact js
    I tried to add js jquery to contact part but could not do it

---
+++ js/contact.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+            $('#submit').click(function() {
+                        fullname = $('#form_name').val();
+                        email = $('#form_email').val();
+                        phone = $('#form_phone').val();
+                        website = $('#form_website').val();
+                        password = $('#form_password').val();
+                        gender = $('#form_gender').val();
+                        message = $('#form_message').val();
+
+                        $(document).ready(function() {
+                                    $(‘#submit’).click(function() {
+                                                fullname = $('#form_name').val();
+                                                sifre = $('#form_password').val();
+                                                uyari = "uyarı";
--- js/style.js
+++
@@ -1,1 +0,0 @@
-import { Button } from 'mdb-ui-kit';
